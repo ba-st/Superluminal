ab802aefaf7017359f3eb5ba01f0bc66df45e927
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2020-11-03 09:13:57 -0300

    Improve test using docker-compose.
    Take into account @serpi90 suggestions.

--- compose-test.sh
+++ compose-test.sh
@@ -7,10 +7,5 @@ if [[ -z "${GITHUB_HEAD_REF##*/}" ]]; then
 else
   echo "BRANCH_NAME=${GITHUB_HEAD_REF##*/}" > .env
 fi
-docker-compose -f api-tests/docker-compose.yml up -d
-echo "Waiting for API client to discover dependencies and exit"
-sleep 15
-EXIT_CODE=$(docker inspect api-tests_api-client_1 --format='{{.State.ExitCode}}')
-echo "Exited with code: $EXIT_CODE"
-docker-compose -f api-tests/docker-compose.yml down
-exit $EXIT_CODE
+set +e # disable exit on error to be able to catch the exit code from compose
+docker-compose -f api-tests/docker-compose.yml up --exit-code-from api-client
