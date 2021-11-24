dee037a130f1bdd2f13ec4d1a17fc556319e523d
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2021-11-24 18:24:33 -0300

    Update docker setup

--- api-tests/start.sh
+++ api-tests/start.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 exec /opt/pharo/pharo \
-	/opt/Superluminal-Service-Discovery-Example/Pharo.image \
-	super-luminal-service-discovery \
-    --message="${MESSAGE}" \
-    --consul-agent-location="${CONSUL_AGENT_LOCATION}" \
-		--retry-delay-in-ms=10000
+	/opt/app/Pharo.image \
+  launchpad start superluminal-service-discovery \
+	--retry-delay-in-ms=10000
